52db687c3c775bd000e848671a48ca51259a26a6
Malcolm Sparks <malcolm@juxt.pro>
2014-01-06 19:07:31 +0000

    project stuff

--- project.clj
+++ project.clj
@@ -3,35 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [liberator "0.10.0"]
                  [com.cemerick/friend "0.2.0"]
 
-
                  [prismatic/dommy "0.1.1"]
                  [cljs-ajax "0.2.3"]
 
-                 ;; Transitive: Because we depend directly on jig/extensions/cljs-builder, remove me when we don't
-                 [org.clojure/clojurescript "0.0-2138"]
-                 ;;[org.clojure/clojurescript "0.0-2030"]
+                 [jig/cljs-builder "2.0.1-SNAPSHOT"]]
 
-                 ]
+  :source-paths ["src" "src-cljs"]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
-
-  ;; This isn't great, but until Malcolm gets the Jig cljs stuff working again
-  :resource-paths ["resources"]
-
-  :source-paths ["src" "src-cljs" "/Users/neale/workspace/jig/extensions/cljs-builder/src"]
-
-  :cljsbuild {
-              :builds [{
-                        ;; The path to the top-level ClojureScript source directory:
-                        :source-paths ["src-cljs"]
-                        ;; The standard ClojureScript compiler options:
-                        ;; (See the ClojureScript compiler documentation for details.)
-                        :compiler {
-                                   :output-to "target/cljsbuild-main.js" ; default: target/cljsbuild-main.js
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
   )
