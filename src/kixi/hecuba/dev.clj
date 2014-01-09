61b26868aa43c8bb84a18723328cb8466ea26063
Malcolm Sparks <malcolm@juxt.pro>
2014-01-09 17:45:25 +0000

    PUT is now a POST

--- src/kixi/hecuba/dev.clj
+++ src/kixi/hecuba/dev.clj
@@ -10,11 +10,11 @@
    (jig Lifecycle)
    (kixi.hecuba.protocols Commander Querier)))
 
-(defn put-resource [uri-prefix data]
+(defn post-resource [post-uri data]
   (http-request
-   {:method :put
-    :url (str uri-prefix (:id data))
-    :body [(:details data)]}
+   {:method :post
+    :url post-uri
+    :body [data]}
    identity))
 
 ;; TODO: Need to depend on routes and use them to path-for the uri, otherwise we tied ourselves to these URIS prematurely
@@ -25,30 +25,33 @@
   (start [_ system]
     (->>
      ;; We could get this sequence from somewhere else
-     [{:id "1" :details {:project-code "IRR"
-                         :name "Eco-retrofit Ealing"
-                         :leaders ["/users/1" "/users/2"]}}
+     [{:project-code "IRR"
+       :name "Eco-retrofit Ealing"
+       :leaders ["/users/1" "/users/2"]}
 
-      {:id "2" :details {:project-code "IRR"
-                         :name "Eco-retrofit Bolton"
-                         :leaders ["/users/1" "/users/2"]}}
+      {:project-code "IRR"
+       :name "Eco-retrofit Bolton"
+       :leaders ["/users/1" "/users/2"]}
 
-      {:id "3" :details {:project-code "IRR"
-                         :name "The Glasgow House"
-                         :leaders ["/users/3"]}}]
+      {:project-code "IRR"
+       :name "The Glasgow House"
+       :leaders ["/users/3"]}]
 
-     (map (partial put-resource "http://localhost:8000/projects/")) ; PUT them over HTTP
+     (map (partial post-resource "http://localhost:8000/projects/")) ; PUT them over HTTP
      (map deref) doall ; wait for all promises to be delivered (all responses to arrive)
-     (every? #(= (:status %) 201))  ; check each returns a status of 201
-     assert)                          ; fail otherwise!
+     #_(every? #(= (:status %) 201))  ; check each returns a status of 201
+     println)                          ; fail otherwise!
     system)
   (stop [_ system] system))
+
+
 
 (deftype RefCommander [r]
   Commander
   (upsert! [_ payload]
     (infof "upserting... %s" payload)
-    (dosync (alter r assoc (:id payload) payload))))
+    (let [id (str (java.util.UUID/randomUUID))]
+      (dosync (alter r assoc id (assoc payload :id id))))))
 
 (defrecord RefQuerier [r]
   Querier
--- src/kixi/hecuba/web.clj
+++ src/kixi/hecuba/web.clj
@@ -31,33 +31,33 @@
            )
   :handle-created (fn [ctx] (::uuid ctx)))
 
-(defresource projects-resource [querier project-resource]
-  :allowed-methods #{:get}
+(defresource projects-resource [querier commander project-resource]
+  :allowed-methods #{:get :post}
   :available-media-types base-media-types
   :exists? (fn [{{{id :id} :route-params body :body routes :jig.bidi/routes} :request :as ctx}]
              {::projects
-              (map (fn [m] (assoc m :href (path-for routes project-resource :id (:id m)))) (items querier))})
+              (map (fn [m]  m #_(assoc m :href (path-for routes project-resource :id (:id m)))) (items querier))})
   :handle-ok (fn [{projects ::projects {mt :media-type} :representation :as ctx}]
                (case mt
                  "text/html" projects ; do something here to render the href as a link
-                 projects)))
-
-(defresource project-resource [querier commander]
-  :allowed-methods #{:get :put}
+                 projects))
+  :post! (fn [{{body :body} :request}]
+          (let [payload (io! (edn/read (java.io.PushbackReader. (io/reader body))))]
+            (upsert! commander payload))))
+(defresource project-resource [querier]
+  :allowed-methods #{:get}
   :available-media-types base-media-types
   :exists? (fn [{{{id :id} :route-params body :body routes :jig.bidi/routes} :request :as ctx}]
              (if-let [p (item querier id)] {::project p} false))
   :handle-ok (fn [ctx] (::project ctx))
-  :put! (fn [{{{id :id} :route-params body :body} :request}]
-          (let [details (io! (edn/read (java.io.PushbackReader. (io/reader body))))]
-            (upsert! commander (assoc details :id id)))))
+  )
 
 (defn index [req]
   {:status 200 :body (slurp (io/resource "hecuba/index.html"))})
 
 (defn make-routes [names producer-config querier commander]
-  (let [project (project-resource querier commander)
-        projects (projects-resource querier project)]
+  (let [project (project-resource querier)
+        projects (projects-resource querier commander project)]
     ["/"
      [["" (->Redirect 307 index)]
       ["overview.html" index]
