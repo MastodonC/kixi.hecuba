5e0bf9a30e4d39ee8571ba5ccf636e58214ac62c
Neale Swinnerton <neale@isismanor.com>
2014-01-15 15:27:08 +0000

    Remove deprecated functions.

--- src-cljs/kixi/hecuba/main_om.cljs
+++ src-cljs/kixi/hecuba/main_om.cljs
@@ -6,35 +6,6 @@
    [cljs.core.async :refer [<! chan put! sliding-buffer]]
    [ajax.core :refer (GET POST)]))
 
-;; Deprecated, just left in to show how to pass through a core.async
-;; channel and when to put stuff on it - from Om counters example
-(defn menuitem [data owner ch]
-  (om/component
-      (dom/li nil
-           (dom/a
-                #js {:onClick
-                     (fn [_] (put! ch (:name (om/read data om/value))))}
-                (:label data)))))
-
-;; Deprecated, just let in to show how and when to run a go block in a
-;; component, and how to pass in the channel into sub-components
-(defn menu [app owner]
-  (let [in (chan (sliding-buffer 1))]
-    (reify
-      om/IWillMount
-      (will-mount [_] (go (while true (let [n (<! in)] (.log js/console "Got an event!" n)))))
-      om/IRender
-      (render [_]
-        (dom/ul #js {:className "dropdown-menu"}
-             #_(.log js/console "Active: " (:active app))
-             (om/build-all menuitem
-                 (:menuitems app)
-                 ;; React says: 'Each child in an array should have a
-                 ;; unique "key" prop. Check the render method of
-                 ;; undefined.'. This is the purpose of the :key entry in
-                 ;; the map below
-                 {:key :name :opts in}))))))
-
 (defn table-row [data owner]
   (om/component
       (dom/tr #js {:onClick (fn [e] (.log js/console "ooh!"))}
