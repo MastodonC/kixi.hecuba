fc4ff70d30e0586e1d1569379467c987953e1a99
Anna Pawlicka <anna@mastodonc.com>
2014-01-15 11:34:15 +0000

    Fix removing of the chart.

--- src-cljs/kixi/hecuba/web/chart.cljs
+++ src-cljs/kixi/hecuba/web/chart.cljs
@@ -3,7 +3,7 @@
    [mrhyde.core :as mrhyde]
    [dommy.core :as dommy])
   (:use-macros
-   [dommy.macros :only [node sel sel1]]))
+   [dommy.macros :only [node sel sel1 by-tag]]))
 
 (def dimple (this-as ct (aget ct "dimple")))
 
@@ -13,6 +13,8 @@
   [div width height]
   (.newSvg dimple div width height))
 
+(def chart-svg)
+
 (defn dimple-chart 
   [div width height data]
   (let [Chart (.-chart dimple)
@@ -21,22 +23,28 @@
 
 (defn init-bar-chart
   [div width height data x-axis-title y-axis-title]
- ; (.log js/console "Height: " height)
   (let [chart (dimple-chart div width height data)]
     (.addCategoryAxis chart "x" x-axis-title)
     (.addMeasureAxis chart "y" y-axis-title)
     (.addSeries chart nil js/dimple.plot.bar)
     (.draw chart)))
 
+(defn nodelist-to-seq
+  "Converts nodelist to (not lazy) seq."
+  [nl]
+  (let [result-seq (map #(.item nl %) (range (.-length nl)))]
+    (doall result-seq)))
+
 (def data  [{"Word" "Hello" "Awesomeness" 2000}
             {"Word" "World" "Awesomeness" 3000}])
 
 (defn reload-chart [e]
- (.log js/console e)
- (.log js/console (dommy/value (sel1 :#devices)))
- (dommy/append! (sel1 :#chart) (init-bar-chart "#chart" 450 350 data "Word" "Awesomeness"))
- )
+  (.log js/console e)
+  (.log js/console (.getElementsByTagName js/document "form"))
+  (.remove (first (nodelist-to-seq (.getElementsByTagName js/document "svg")))))
 
-(dommy/append! (sel1 :#title) (node [:h3 "Metering data"]))
 (dommy/listen! (sel1 :#submit) :click reload-chart)
-;(init-bar-chart "#chart" 450 350 data "Word" "Awesomeness")
+
+(set! chart-svg (init-bar-chart "#chart" 450 350 data "Word" "Awesomeness"))
+
+
