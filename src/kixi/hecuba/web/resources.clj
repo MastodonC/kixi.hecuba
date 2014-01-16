f3fa7a73df43cb62d942c3ba10b913057aa63a54
Malcolm Sparks <malcolm@juxt.pro>
2014-01-16 14:03:54 +0000

    Lots of commenting added to the resources namespace, because some of the coding is subtle.

--- src/kixi/hecuba/web/resources.clj
+++ src/kixi/hecuba/web/resources.clj
@@ -12,58 +12,105 @@
 
 (def base-media-types ["text/html" "application/json" "application/edn"])
 
-(defresource items-resource [typ querier commander detail-resource]
+(defn render-items-html
+  "Render some HTML for the text/html representation. Mostly this is for
+  debug, to be able to check the data without too much UI logic
+  involved."
+  [items]
+  (let [fields (remove #{:hecuba/name :hecuba/id :hecuba/href :hecuba/type}
+                       (distinct (mapcat keys items)))]
+    (let [DEBUG false]
+      (html [:body
+             [:h2 "Fields"]
+             [:ul (for [k fields] [:li (csk/->snake_case_string (name k))])]
+             [:h2 "Items"]
+             [:table
+              [:thead
+               [:tr
+                [:th "Name"]
+                (for [k fields] [:th (string/replace (csk/->Snake_case_string k) "_" " ")])
+                (when DEBUG [:th "Debug"])]]
+              [:tbody
+               (for [p items]
+                 [:tr
+                  [:td [:a {:href (:hecuba/href p)} (:hecuba/name p)]]
+                  (for [k fields] [:td (str (k p))])
+                  (when DEBUG [:td (pr-str p)])])]]]))))
+
+;; Now for the Liberator resources. These are heavily commented because
+;; Liberator isn't well-known. Check the Liberator website for more
+;; info: http://clojure-liberator.github.io/liberator/
+
+;; REST resource for items (plural) - .
+(defresource items-resource [typ querier commander item-resource]
+  ;; acts as both an index of existing items and factory for new ones
   :allowed-methods #{:get :post}
 
+  :exists? true ; This 'factory' resource ALWAYS exists
   :available-media-types base-media-types
 
-  ;; This 'factory' resource ALWAYS exists, but we piggyback on this decision point to find the items.
-  ;; TODO: don't do this!
-  :exists? (fn [{{route-params :route-params body :body routes :jig.bidi/routes} :request :as ctx}]
-             {::items
-              (map (fn [m] (assoc m :hecuba/href (path-for routes detail-resource :hecuba/id (:hecuba/id m))))
-                   (items querier (merge route-params {:hecuba/type typ})))})
+  :handle-ok ; do this upon a successful GET
+  (fn [{{mime :media-type} :representation {routes :jig.bidi/routes route-params :route-params} :request}]
+    (let [items
+          (->>
+           {:hecuba/type typ} ; form a 'where' clause starting with the type
+           (merge route-params) ; adding any route-params
+           (items querier) ; to query items
+           ;; which are adorned with hrefs, throwing bidi's path-for all the kv pairs we have!
+           (map #(assoc % :hecuba/href (apply path-for routes item-resource (apply concat %)))))]
+      (case mime
+        "text/html" (render-items-html items)
+        ;; Liberator's default rendering of application/edn seems wrong
+        ;; (it wraps the data in 'clojure.lang.PersistentArrayMap', so
+        ;; we override it here.
+        "application/edn" (pr-str (vec items))
 
-  :handle-ok (fn [{items ::items {mt :media-type} :representation :as ctx}]
-               (case mt
-                 "text/html"
-                 (let [debug false
-                       fields (remove #{:hecuba/name :hecuba/id :hecuba/href :hecuba/type}
-                                      (distinct (mapcat keys items)))]
-                   (html [:body
-                          [:h2 "Fields"]
-                          [:ul (for [k fields] [:li (csk/->snake_case_string (name k))])]
-                          [:h2 "Items"]
-                          [:table
-                           [:thead
-                            [:tr
-                             [:th "Name"]
-                             (for [k fields] [:th (string/replace (csk/->Snake_case_string k) "_" " ")])
-                             (when debug [:th "Debug"])]]
-                           [:tbody
-                            (for [p items]
-                              [:tr
-                               [:td [:a {:href (:hecuba/href p)} (:hecuba/name p)]]
-                               (for [k fields] [:td (str (k p))])
-                               (when debug [:td (pr-str p)])])]]]))
-                 "application/edn" (pr-str (vec items))
-                 items))
+        ;; The default is to let Liberator render our data
+        items)))
 
-  :handle-created (fn [{{routes :jig.bidi/routes} :request id :hecuba/id :as ctx}]
-                    (ring-response
-                     {:headers {"Location"
-                                (path-for routes detail-resource :hecuba/id id)}}))
+  :post! ; enact the 'side-effect' of creating an item
+  (fn [{{body :body} :request}]
+    {:hecuba/id ; add this entry to Liberator's context
+     (upsert! commander
+              (-> body
+                  ;; Prepare to read the body
+                  io/reader (java.io.PushbackReader.)
+                  ;; Read the body (can't repeat this so no transactions!)
+                  edn/read io!
+                  ;; Add the type prior to upserting
+                  (assoc :hecuba/type typ)))})
 
-  :post! (fn [{{body :body} :request}]
-           (let [payload (io! (edn/read (java.io.PushbackReader. (io/reader body))))]
-             {:hecuba/id (upsert! commander (assoc payload :hecuba/type typ))})))
+  :handle-created ; do this upon a successful POST
+  (fn [{{routes :jig.bidi/routes route-params :route-params} :request id :hecuba/id}]
+    (ring-response ; Liberator, we're returning an actual Ring map!
+     ;; We must return the new resource path in the HTTP Location
+     ;; header. Liberator doesn't do this for us, and clients might need
+     ;; to know where the new resource is located.
+     ;; See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.2.2
+     {:headers {"Location" (path-for routes item-resource (assoc route-params :hecuba/id id))}})))
 
+(defn render-item-html
+  "Render an item as HTML"
+  [item]
+  (html
+   [:body
+    [:h1 (:hecuba/name item)]
+    [:pre (pr-str item)]]))
+
+;; REST resource for individual items.
 (defresource item-resource [querier]
   :allowed-methods #{:get}
   :available-media-types base-media-types
-  :exists? (fn [{{{id :hecuba/id} :route-params body :body routes :jig.bidi/routes} :request :as ctx}]
-             (if-let [p (item querier id)] {::item p} false))
-  :handle-ok (fn [{item ::item {mt :media-type} :representation :as ctx}]
-               (case mt
-                 "text/html" (html [:h1 (:hecuba/name item)])
-                 item)))
+
+  :exists? ; iff the item exists, we bind it to ::item in order to save
+           ; an extra query later on - this is a common Liberator
+           ; pattern
+  (fn [{{{id :hecuba/id} :route-params body :body routes :jig.bidi/routes} :request}]
+    (when-let [itm (item querier id)] {::item itm}))
+
+  :handle-ok
+  (fn [{item ::item {mime :media-type} :representation}]
+    (case mime
+      "text/html" (render-item-html item)
+      ;; The default is to let Liberator render our data
+      item)))
