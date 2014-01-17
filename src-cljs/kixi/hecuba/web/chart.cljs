5c6c0a20ef6e6665a08983eb5ed098def790f5a4
Anna Pawlicka <anna@mastodonc.com>
2014-01-17 14:55:39 +0000

    Fix dynamic form

--- src-cljs/kixi/hecuba/web/chart.cljs
+++ src-cljs/kixi/hecuba/web/chart.cljs
@@ -23,18 +23,18 @@
 
 ;;;;;;;;;;; Data ;;;;;;;;;;;
 
-(def mock-data {:external-humidity     [{"month" "january" "reading" 0.8}
-                                        {"month" "february" "reading" 0.9}
-                                        {"month" "march" "reading" 0.8}
-                                        {"month" "april" "reading" 0.75}
-                                        {"month" "may" "reading" 0.65}
-                                        {"month" "june" "reading" 0.50}
-                                        {"month" "july" "reading" 0.55}
-                                        {"month" "august" "reading" 0.6}
-                                        {"month" "september" "reading" 0.66}
-                                        {"month" "october" "reading" 0.68}
-                                        {"month" "november" "reading" 0.71}
-                                        {"month" "december" "reading" 0.9}]
+(def mock-data {:external-humidity     [{"month" "Jan" "reading" 0.8}
+                                        {"month" "Feb" "reading" 0.9}
+                                        {"month" "Mar" "reading" 0.8}
+                                        {"month" "Apr" "reading" 0.75}
+                                        {"month" "May" "reading" 0.65}
+                                        {"month" "Jun" "reading" 0.50}
+                                        {"month" "Jul" "reading" 0.55}
+                                        {"month" "Aug" "reading" 0.6}
+                                        {"month" "Sep" "reading" 0.66}
+                                        {"month" "Oct" "reading" 0.68}
+                                        {"month" "Nov" "reading" 0.71}
+                                        {"month" "Dec" "reading" 0.9}]
                 :external-temperature  [{"month" "january" "reading" 6}
                                         {"month" "february" "reading" 10}
                                         {"month" "march" "reading" 12}
@@ -64,63 +64,77 @@
 ;;;;;;;;;;; List of devices for axis plots ;;;;;;;;;;
 
 (defn device-list-item
-  [app {:keys [n]}]
-  (.log js/console "device-list-item")
-  (.log js/console (str "N: " n))
-  (om/component
-   (let [device   (get-in app [:devices n])
-         selected (= n (:selected app))]
-     (.log js/console (:name device))
-     (dom/input #js {:id (:id device) :type "checkbox" :onClick #(select-device app n)} (:name device))
-     )))
-
-(defn devices-list [app]
-  (om/component
-   (.log js/console "devices-list")
-   (dom/div #js {:id "devices"}
-            (dom/h4 nil "Devlices")
-            (dom/form nil (into-array
-                           (map #(om/build device-list-item
-                                           app
-                                           {:opts {:key %}})
-                                (range (count (:devices app)))))))))
+  [devices]
+  (fn [data owner]
+    (.log js/console "device-list-item")
+    (om/component
+     (apply dom/input #js {:type "checkbox" :onClick #(select-device app (:hecuba/name device))}
+            (for [device devices]
+              (str (get data device)))))))
 
 ;;;;;;;;;; Chart UI ;;;;;;;;;;
 
-(defn chart-view [app opts]
+(defn chart-view [data opts]
   (reify
    om/IRender
    (render [this] (dom/div #js {:id "chart"}))
    om/IDidMount
    (did-mount [_ owner]
      (let [Chart        (.-chart dimple)
-           svg          (.newSvg dimple "#chart" 450 450)
+           svg          (.newSvg dimple "#chart" 400 350)
            dimple-chart (Chart. svg (clj->js (:external-humidity mock-data)))]
-       (.addOrderRule (.addCategoryAxis dimple-chart "x" "month") "Date")
+       (.setBounds dimple-chart 60 30 300 300)
+       (.addCategoryAxis dimple-chart "x" "month")
        (.addMeasureAxis dimple-chart "y" "reading")
        (.addSeries dimple-chart nil js/dimple.plot.line)
        (.draw dimple-chart)))))
 
+(defn chart-item
+  [data]
+  (fn [data opts]
+    (reify
+      om/IRender
+      (render [this]
+        (.log js/console "[chart] I render")
+        (dom/div #js {:id "chart"}))
+      om/IDidMount
+      (did-mount [_ owner]
+        (let [Chart        (.-chart dimple)
+              svg          (.newSvg dimple "#chart" 400 350)
+              dimple-chart (Chart. svg (clj->js (:external-humidity mock-data)))]
+          (.setBounds dimple-chart 60 30 300 300)
+          (.addCategoryAxis dimple-chart "x" "month")
+          (.addMeasureAxis dimple-chart "y" "reading")
+          (.addSeries dimple-chart nil js/dimple.plot.line)
+          (.draw dimple-chart) )))))
+
 ;;;;;;;;;;; Bootstrap ;;;;;;;;;;;;
 
-(defn chart-ui [app]
-  (om/component
-   (dom/div nil
-            (dom/header #js {:className "auth"})
-            (dom/div #js {:id "data"}
-                     (om/build devices-list app {:opts [:devices (:selected app)]}))
-            (om/build chart-view app {:opts [:layers]}))))
+(defn create-form-and-chart [model-path device-item chart-item]
+  (fn [data owner]
+    (reify
+      om/IRender
+      (render [_]
+        (om/build chart-item data)
+        (dom/div #js {:className "devices"}
+                 (dom/form #js {:className "devices-form"}
+                           (om/build-all device-item
+                                         (get-in data model-path)
+                                         {:key :hecuba/name})))))))
 
 
-(go (let [external-temp     {:name "External temperature"
-                             :id "external_temp"
+(go (let [external-temp     {:hecuba/name "01"
+                             :name "External temperature"
                              :data (:external-temperature mock-data)}
-          external-humidity {:name "External humidity"
-                             :id "external_humidity"
+          external-humidity {:hecuba/name "02"
+                             :name "External humidity"
                              :data (:external-humidity mock-data)}]
       (let [init-state (update-in app-state [:devices]
                                   #(vec (concat % [external-temp external-humidity])))]
-        (om/root init-state chart-ui (.getElementById js/document "app")))))
+        (om/root init-state (create-form-and-chart [:devices]
+                                                   (device-list-item [:hecuba/name :name])
+                                                   (chart-item [:hecuba/name :name]))
+                 (.getElementById js/document "app")))))
 
 
 
