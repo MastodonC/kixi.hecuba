82e6c27d70ef1f543e1a819c2e8b9df52c86fc9a
Malcolm Sparks <malcolm@juxt.pro>
2014-01-20 22:27:43 +0000

    Rename card to tab

--- src-cljs/kixi/hecuba/main_om.cljs
+++ src-cljs/kixi/hecuba/main_om.cljs
@@ -29,18 +29,18 @@
                      (om/build-all table-row (:rows data)
                          {:key :hecuba/id :opts (:cols data)}))))))
 
-(defn blank-card [data owner]
+(defn blank-tab [data owner]
   (om/component
       (dom/p nil "This page is unintentionally left blank")))
 
-(defn about-card [data owner card]
+(defn about-tab [data owner tab]
   (om/component
-      (dom/p nil "I'm the About card")))
+      (dom/p nil "I'm the About tab")))
 
-(defn programmes-card [data owner card]
+(defn programmes-tab [data owner tab]
   (om/component
       (dom/div nil
-           (dom/h1 nil (:title card))
+           (dom/h1 nil (:title tab))
            (when-let [tabdata (get-in data [:programmes :table])]
              (om/build table tabdata))
            (dom/h1 nil "Projects")
@@ -50,14 +50,14 @@
            (when-let [tabdata (get-in data [:properties :table])]
              (om/build table tabdata)))))
 
-(defn card-container [cards]
+(defn tab-container [tabs]
   (fn [data owner]
     (om/component
         (dom/div nil
-             (let [selected (-> data :card-container :selected)]
-               (if-let [card (get cards selected)]
-                 (om/build card data {:opts (->> data :card-container :cards (filter #(= (:name %) selected)) first)})
-                 (om/build blank-card data)))))))
+             (let [selected (-> data :tab-container :selected)]
+               (if-let [tab (get tabs selected)]
+                 (om/build tab data {:opts (->> data :tab-container :tabs (filter #(= (:name %) selected)) first)})
+                 (om/build blank-tab data)))))))
 
 
 (def app-model
@@ -73,8 +73,8 @@
                            {:name :documentation :label "Documentation"}
                            {:name :api_users :label "API users"}
                            ]}
-         :card-container {:selected :programmes
-                          :cards [{:name :about :title "About"}
+         :tab-container {:selected :programmes
+                          :tabs [{:name :about :title "About"}
                                   {:name :documentation :title "Documentation"}
                                   {:name :users :title "Users"}
                                   {:name :programmes :title "Programmes"}
@@ -85,14 +85,14 @@
 
 (defn ^:export handle-left-nav [menu-item]
   ;; Currently we implement a one-to-one correspondence between the
-  ;; left-hand-menu and the card container, but in due course 'Project'
+  ;; left-hand-menu and the tab container, but in due course 'Project'
   ;; and 'Properties' will cause a scroll to a location under the
-  ;; 'Programmes' card
-  (swap! app-model assoc-in [:card-container :selected] menu-item))
+  ;; 'Programmes' tab
+  (swap! app-model assoc-in [:tab-container :selected] menu-item))
 
 (defn ^:export change []
   (.log js/console "Hello Malcolm!")
-  (change-card "documentation")
+  (change-tab "documentation")
   )
 
 ;; Add navigation.
@@ -107,8 +107,8 @@
                                  ["Name" "Address" "Rooms" "Construction date"]
                                  (make-table-row [:hecuba/name :address :rooms :date-of-construction])) (.getElementById js/document "hecuba-properties"))
 
-(om/root app-model (card-container {:about about-card
-                                    :programmes programmes-card}) (.getElementById js/document "hecuba-cards"))
+(om/root app-model (tab-container {:about about-tab
+                                    :programmes programmes-tab}) (.getElementById js/document "hecuba-tabs"))
 
 (GET "/messages/" {:handler (fn [x]
                               (println "Messages: " x)
