6d91a001a7ff97f69b1dfc525fb1cb32962ac191
Malcolm Sparks <malcolm@juxt.pro>
2014-01-27 13:17:12 +0000

    Better documentation on existing test suite

--- test/kixi/hecuba/dev_test.clj
+++ test/kixi/hecuba/dev_test.clj
@@ -3,12 +3,21 @@
         kixi.hecuba.protocols
         kixi.hecuba.dev))
 
+
 (deftest query
   (let [r (ref {})
         commander (->RefCommander r)
         querier (->RefQuerier r)]
+
+    ;; Add some data
     (upsert! commander {:hecuba/name "Project X" :hecuba/type :project})
     (upsert! commander {:hecuba/name "Project Y" :hecuba/type :project})
     (upsert! commander {:hecuba/name "Falling Water" :hecuba/type :property})
-    (is (= (count (items querier)) 3))
-    (is (= (count (items querier {:hecuba/type :project})) 2))))
+
+    (is (= (count (items querier)) (count @r))
+        "Without a constraint argument, items should return all the items in the ref")
+
+    (is (= (count (items querier {:hecuba/type :project}))
+           (count (filter #(= :project (:hecuba/type %)) (vals @r))))
+        "With a constraint, just the items that satisfy the constraint"
+        )))
