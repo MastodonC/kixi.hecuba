18b7c5bcac61cebb3f9ea4bad401d2748f4fa99b
Malcolm Sparks <malcolm@juxt.pro>
2014-02-04 14:32:38 +0000

    Improvement to AMON API delete entity Liberator resource

--- src/kixi/hecuba/web/amon.clj
+++ src/kixi/hecuba/web/amon.clj
@@ -1,6 +1,6 @@
 (ns kixi.hecuba.web.amon
   (:require
-   [kixi.hecuba.protocols :refer (upsert! delete!)]
+   [kixi.hecuba.protocols :refer (upsert! delete! item)]
    jig
    [bidi.bidi :refer (path-for)]
    [jig.bidi :refer (add-bidi-routes)]
@@ -22,19 +22,15 @@
            {:hecuba/id (upsert! commander (assoc (decode body) :hecuba/id (make-uuid)))})
   :handle-created (fn [{{routes :jig.bidi/routes} :request id :hecuba/id}]
                     (ring-response
-                     {:headers {"Location" (path-for routes (:entities-specific @handlers) :hecuba/id id)}})
-                    ))
+                     {:headers {"Location" (path-for routes (:entities-specific @handlers) :hecuba/id id)}})))
 
 (defresource entities-specific [{:keys [commander querier]} handlers]
   :allowed-methods #{:delete}
   :available-media-types #{"application/json"}
   :exists? (fn [{{{id :hecuba/id} :route-params} :request}]
-             (delete! commander (UUID/fromString id))
-
-)
-  :delete! (fn [ctx] nil)
-
-)
+             {::item (item querier (UUID/fromString id))})
+  :delete! (fn [{{id :hecuba/id} ::item}]
+             (delete! commander id)))
 
 ;; Routes
 
