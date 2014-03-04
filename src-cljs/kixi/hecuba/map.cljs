7fe43f16f2afd0ed69a14aa26d8da0cb6faae817
Malcolm Sparks <malcolm@juxt.pro>
2014-03-04 11:20:15 +0000

    Moved map to correspond to ns

--- src-cljs/kixi/hecuba/map.cljs
+++ src-cljs/kixi/hecuba/map.cljs
@@ -1,4 +1,4 @@
-(ns kixi.hecuba.web.map
+(ns kixi.hecuba.map
   (:require
    [mrhyde.core :as mrhyde]
    ))
@@ -10,7 +10,7 @@
 (def tile-url
   "http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/22677/256/{z}/{x}/{y}.png")
 
-(let [mappy (-> L (.map "mappy") 
+(let [mappy (-> L (.map "mappy")
                   (.setView [53.0, -1.5] 6))]
 
   (-> L (.tileLayer tile-url
@@ -18,7 +18,7 @@
                      :attribution "Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2012 CloudMade"})
         (.addTo mappy))
 
-  (-> L (.marker [53.0086, -1.5]) 
+  (-> L (.marker [53.0086, -1.5])
         (.addTo mappy)
         (.bindPopup "<b>Hello world!</b><br />I am a popup.")
         (.openPopup))
