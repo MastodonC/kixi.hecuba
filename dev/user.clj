057cfad6b20bc25698defd2c8a7aab131c76a0e9
Neale Swinnerton <neale@isismanor.com>
2014-04-10 14:27:08 +0100

    Ensure REPL will start with compilation errors.
    Add some gymnastics to make sure we can get the REPL up even with errors
    in our code.

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,14 @@
 (ns user
   (:require
+
+
+   ;; DO NOT ADD ANYTHING HERE THAT MIGHT REMOTELY HAVE A COMPILATION ERROR.
+   ;; THIS IS TO ENSURE WE CAN ALWAYS GET A REPL STARTED.
+   ;;
+   ;; see (init) below.
+
    [com.stuartsierra.component :as component]
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-   [kixi.hecuba.application.system :refer (new-system)]
    [clojure.pprint :refer (pprint)]
    [clojure.reflect :refer (reflect)]
    [clojure.repl :refer (apropos dir doc find-doc pst source)]
@@ -11,8 +17,13 @@
 (defn init
   "Constructs the current development system."
   []
-  (alter-var-root #'system
-    (constantly (new-system))))
+  ;; We do some gymnastics here to make sure that the REPL can always start
+  ;; even in the presence of compilation errors.
+  (require '[kixi.hecuba.application.system])
+
+  (let [new-system (resolve 'kixi.hecuba.application.system/new-system)]
+    (alter-var-root #'system
+                    (constantly (new-system)))))
 
 (defn start
   "Starts the current development system."
