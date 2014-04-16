645f7e351a19306d451f7b1be1d73e213cfddf72
Neale Swinnerton <neale@isismanor.com>
2014-04-16 17:05:48 +0100

    properties - 1st cut of rejig to new code style.

--- src/kixi/hecuba/api/properties.clj
+++ src/kixi/hecuba/api/properties.clj
@@ -4,30 +4,33 @@
    [cheshire.core :as json]
    [clojure.tools.logging :as log]
    [kixi.hecuba.protocols :as hecuba]
-   [kixi.hecuba.webutil :as util]
-   [kixi.hecuba.webutil :refer (decode-body authorized? uuid stringify-values sha1-regex)]
-   [liberator.core :refer (defresource)]
-   ))
+   [kixi.hecuba.webutil :refer (decode-body authorized? uuid stringify-values sha1-regex routes-from ) :as util]
+   [liberator.core :refer (defresource)]))
+
+(defn- project-id-from [ctx]
+  (get-in [:request :route-params :project-id]))
+
+(defn index-handle-ok [querier handlers ctx]
+  (let [request (:request ctx)]
+    (let [coll (->> (if (project-id-from ctx)
+                      (hecuba/items querier :entity (:route-params request))
+                      (hecuba/items querier :entity))
+                    (map #(-> %
+                              (assoc :device-ids (map :id (hecuba/items querier :device {:entity-id (:id %)}))
+                                     :href (bidi/path-for (routes-from ctx)
+                                                          (:entity @handlers) :entity-id (:id %))))))
+          scoll (sort-by :address-street-two coll)]
+
+      (case (get-in ctx [:representation :media-type])
+        "text/html" (util/render-items request scoll)
+        "application/json" (->> scoll
+                                (map util/downcast-to-json)
+                                (map util/camelify)
+                                json/encode)
+        scoll))))
 
 (defresource index [{:keys [commander querier]} handlers]
   :allowed-methods #{:get}
   :available-media-types #{"application/json" "application/edn" "text/html"}
   :authorized? (authorized? querier :property)
-
-  :handle-ok
-  (fn [{{mime :media-type} :representation {routes :modular.bidi/routes route-params :route-params} :request :as req}]
-      (let [coll (->> (if (:project-id route-params)
-                      (hecuba/items querier :entity route-params)
-                      (hecuba/items querier :entity))
-                      (map #(-> %
-                                (assoc :device-ids (map :id (hecuba/items querier :device {:entity-id (:id %)})))
-                                (assoc :href (bidi/path-for routes (:entity @handlers) :entity-id (:id %))))))
-            scoll (sort-by :address-street-two coll)]
-
-        (case mime
-          "text/html" (util/render-items req scoll)
-          "application/json" (->> scoll
-                                  (map util/downcast-to-json)
-                                  (map util/camelify)
-                                  json/encode)
-          scoll))))
+  :handle-ok (partial index-handle-ok querier handlers))
