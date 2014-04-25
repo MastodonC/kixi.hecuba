0f9a2c3c82cd7472db813c1987dbe35c616c8807
Bruce Durling <bruce@mastodonc.com>
2014-04-25 17:44:02 +0100

    Updated lein-cljsbuild, clojurescript & deps
    Some project.clj getting rid of dupe libraries with some exclusions.

--- project.clj
+++ project.clj
@@ -4,42 +4,43 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
 
-                 [org.clojure/clojurescript "0.0-2173" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2202"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha" :scope "provided"]
-
+                 
                  [kixi/bidi "1.10.3-p0-b569a50"]
-
+                 
                  ;; Modular
                  [juxt/modular "0.2.0"]
                  [juxt.modular/http-kit "0.2.0"]
                  [juxt.modular/bidi "0.2.1"]
-                 [cylon "0.1.0"]
-
+                 [cylon "0.1.0" :exclusions [ring/ring-core]]
+                 
                  ;; EDN reader with location metadata - for configuration
                  [org.clojure/tools.reader "0.8.3"]
-
+                 
                  ;; using a patched liberator pending
                  ;; https://github.com/clojure-liberator/liberator/pull/120
                  [kixi/liberator "0.12.0-p2-413ee59"]
                  [cheshire "5.3.1"]
-
+                 
                  ;; Required for Cassandra (possibly only OSX)
                  [org.xerial.snappy/snappy-java "1.1.0.1"]
-
+                 
                  ;; ClojureScript dependencies
                  [prismatic/dommy "0.1.2"]
                  [cljs-ajax "0.2.3"]
                  [om "0.6.1"]
                  [net.drib/mrhyde "0.5.3"]
                  [com.andrewmcveigh/cljs-time "0.1.3"]
-
-                 [clj-kafka "0.2.0-0.8" :exclusions [org.slf4j/slf4j-simple]]
+                 [sablono "0.2.16"]
+                 
+                 [clj-kafka "0.2.0-0.8" :exclusions [org.slf4j/slf4j-simple org.apache.zookeeper/zookeeper]]
                  [camel-snake-kebab "0.1.4"]
-                 [cc.qbits/alia "2.0.0-rc1"]
+                 [cc.qbits/alia "2.0.0-rc1" :exclusions [com.google.guava/guava org.flatland/useful]]
                  [cc.qbits/hayt "2.0.0-beta4"]
 
 
@@ -70,7 +71,8 @@
              
              :uberjar {:main kixi.hecuba.controller.main :aot [kixi.hecuba.controller.main]}}
 
-  :exclusions [[org.clojure/clojurescript]
+  :exclusions [[org.clojure/clojure]
+               [org.clojure/clojurescript]
                [org.clojure/core.async]
                [bidi]
                ]
