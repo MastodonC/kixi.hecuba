55dfef848caeb3de2fecdd9409dc89557e2e08e1
Riccardo Cambiassi <r.cambiassi@gmail.com>
2014-05-07 10:35:37 +0100

    cleanup project.clj

--- project.clj
+++ project.clj
@@ -10,26 +10,26 @@
 
                  [org.clojure/clojurescript "0.0-2202"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha" :scope "provided"]
-                 
+
                  [kixi/bidi "1.10.3-p0-b569a50"]
-                 
+
                  ;; Modular
                  [juxt/modular "0.2.0"]
                  [juxt.modular/http-kit "0.2.0"]
                  [juxt.modular/bidi "0.2.1"]
                  [cylon "0.1.0" :exclusions [ring/ring-core]]
-                 
+
                  ;; EDN reader with location metadata - for configuration
                  [org.clojure/tools.reader "0.8.3"]
-                 
+
                  ;; using a patched liberator pending
                  ;; https://github.com/clojure-liberator/liberator/pull/120
                  [kixi/liberator "0.12.0-p2-413ee59"]
                  [cheshire "5.3.1"]
-                 
+
                  ;; Required for Cassandra (possibly only OSX)
                  [org.xerial.snappy/snappy-java "1.1.0.1"]
-                 
+
                  ;; ClojureScript dependencies
                  [prismatic/dommy "0.1.2"]
                  [cljs-ajax "0.2.3"]
@@ -37,7 +37,7 @@
                  [net.drib/mrhyde "0.5.3"]
                  [com.andrewmcveigh/cljs-time "0.1.3"]
                  [sablono "0.2.16"]
-                 
+
                  [clj-kafka "0.2.0-0.8" :exclusions [org.slf4j/slf4j-simple org.apache.zookeeper/zookeeper]]
                  [camel-snake-kebab "0.1.4"]
                  [cc.qbits/alia "2.0.0-rc1" :exclusions [com.google.guava/guava org.flatland/useful]]
@@ -68,7 +68,7 @@
                                   [org.clojure/tools.namespace "0.2.4"]
                                   ]
                    :plugins [[com.cemerick/austin "0.1.4"]] }
-             
+
              :uberjar {:main kixi.hecuba.controller.main :aot [kixi.hecuba.controller.main]}}
 
   :exclusions [[org.clojure/clojure]
