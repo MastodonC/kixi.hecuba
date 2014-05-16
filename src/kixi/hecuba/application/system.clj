25cfbdb73f32b0c2e43225aa4ae593468ec19f68
Anna Pawlicka <anna@mastodonc.com>
2014-05-16 14:22:45 +0100

    Combine default and server hecuba.edn files.

--- src/kixi/hecuba/application/system.clj
+++ src/kixi/hecuba/application/system.clj
@@ -31,12 +31,31 @@
    [clojure.tools.reader.reader-types :refer (indexing-push-back-reader source-logging-push-back-reader)]
    [clojure.java.io :as io]))
 
+(defn combine
+  "Merge maps, recursively merging nested maps whose keys collide."
+  ([] {})
+  ([m] m)
+  ([m1 m2]
+    (reduce (fn [m1 [k2 v2]]
+              (if-let [v1 (get m1 k2)]
+                (if (and (map? v1) (map? v2))
+                  (assoc m1 k2 (combine v1 v2))
+                  (assoc m1 k2 v2))
+                (assoc m1 k2 v2)))
+            m1 m2))
+  ([m1 m2 & more]
+    (apply combine (combine m1 m2) more)))
+
 (defn config []
   (let [f (io/file (System/getProperty "user.home") ".hecuba.edn")]
     (when (.exists f)
-      (clojure.tools.reader/read
-       (indexing-push-back-reader
-        (java.io.PushbackReader. (io/reader f)))))))
+      (combine
+       (clojure.tools.reader/read
+        (indexing-push-back-reader
+         (java.io.PushbackReader. (io/reader "resources/default.hecuba.edn")))) ;; TODO change path once we deploy from jar
+       (clojure.tools.reader/read
+        (indexing-push-back-reader
+         (java.io.PushbackReader. (io/reader f)))))))) 
 
 
 (defn define-modules [state]
