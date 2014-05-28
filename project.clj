c3acd052fb1b307c39f83425cfa375d1586a29e5
Bruce Durling <bruce@mastodonc.com>
2014-05-28 23:13:58 +0100

    Update Om.

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                  ;; ClojureScript dependencies
                  [prismatic/dommy "0.1.2"]
                  [cljs-ajax "0.2.3"]
-                 [om "0.6.1"]
+                 [om "0.6.4"]
                  [net.drib/mrhyde "0.5.3"]
                  [com.andrewmcveigh/cljs-time "0.1.3"]
                  [sablono "0.2.17"]
