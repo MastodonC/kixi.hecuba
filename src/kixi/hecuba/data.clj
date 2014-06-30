68069b22a5a5e3957548c831ba1d0831fba90fb1
Bruce Durling <bruce@mastodonc.com>
2014-06-30 10:11:47 +0100

    Added explanatory text as per sw1nn's request.

--- src/kixi/hecuba/data.clj
+++ src/kixi/hecuba/data.clj
@@ -3,12 +3,18 @@
 
 ;; Some utility funcitons
 
-(defn parse-list [m key]
+(defn parse-list
+  "Add a json list to a map with the given key if the list exists,
+  otherwise return the map unchanged."
+  [m key]
   (if-let [items (get m key)]
     (assoc m key (mapv #(json/parse-string % keyword) items))
     m))
 
-(defn parse-item [m key]
+(defn parse-item
+  "Add a json object to a map with the given key if the list exists,
+  otherwise return the map unchanged."
+  [m key]
   (if-let [item (get m key)]
     (assoc m key (json/parse-string item keyword))
     m))
