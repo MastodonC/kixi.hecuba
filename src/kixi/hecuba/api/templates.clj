7793e535b9320e57e89276e56a53c4ee7b4b3cc2
Neale Swinnerton <neale@isismanor.com>
2014-07-14 13:20:28 +0100

    refactor templates data handling and http apart.

--- src/kixi/hecuba/api/templates.clj
+++ src/kixi/hecuba/api/templates.clj
@@ -11,8 +11,9 @@
             [kixi.hecuba.webutil :refer (request-method-from-context decode-body authorized? stringify-values update-stringified-lists sha1-regex uuid)]
             [liberator.core :refer (defresource)]
             [liberator.representation :refer (ring-response)]
-            [qbits.hayt :as hayt])
-  )
+            [qbits.hayt :as hayt]
+            [kixi.hecuba.data.measurements.download :as measurements-download]))
+
 (def ^:private templates-resource (p/resource-path-string :templates-resource))
 (def ^:private entity-templates-resource (p/resource-path-string :entity-templates-resource))
 
@@ -32,49 +33,6 @@
        content-type
        filename
        name))
-
-(defn- relocate-customer-ref [m]
-  (-> m
-      (dissoc :metadata)
-      (assoc :customer_ref (get-in m [:metadata :customer_ref]))))
-
-(defn- relocate-location [m]
-  (assoc m :location (get-in m [:location :name])))
-
-(def columns-in-order [:device_id
-                       :type
-                       :customer_ref
-                       :description
-                       :location
-                       :accuracy
-                       :resolution
-                       :frequency
-                       :period
-                       :entity_id ;; NOTE: This is shown as "Parent
-                                  ;; UUID" in the samples. Assuming
-                                  ;; parent_id column is old API and
-                                  ;; using entity_id instead
-                       :max
-                       :min])
-
-(def ^:private extract-columns-in-order (apply juxt columns-in-order))
-
-(def ^:private headers-in-order ["Device UUID"
-                                 "Reading Type"
-                                 "Customer Ref"
-                                 "Description"
-                                 "Location"
-                                 "Accuracy (percent)"
-                                 "Sample Interval (seconds)"
-                                 "Frequency"
-                                 "Period"
-                                 "Parent UUID"
-                                 "Sensor Range Max"
-                                 "Sensor Range Min"])
-
-(defn- join-to-device [devices x]
-  (let [device_id (:device_id x)]
-    (merge (get devices device_id) x)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; RESOURCE FUNCTIONS
@@ -184,23 +142,7 @@
 (defn entity-resource-handle-ok [store ctx]
   (db/with-session [session (:hecuba-session store)]
     (let [entity_id       (:id (:kixi.hecuba.api.entities/item ctx))
-          devices         (->> (db/execute session (hayt/select :devices (hayt/where [[= :entity_id entity_id]])))
-                               (map (fn [x] [(:id x) x]))
-                               (into {}))
-          sensors         (if (seq devices)
-                            (db/execute session (hayt/select :sensors (hayt/where [[:in :device_id (keys devices)]])))
-                            [])
-          device-and-type #(str (:device_id %) "-" (:type %))
-          items           (if (seq sensors) 
-                            (->> sensors
-                                 (map (partial join-to-device devices))
-                                 (sort-by device-and-type)
-                                 (map relocate-customer-ref)
-                                 (map relocate-location)
-                                 (map extract-columns-in-order)
-                                 (apply map vector)
-                                 (map #(apply vector %1 %2) headers-in-order))
-                            [])]
+          items           (measurements-download/get-header entity_id)]
       (ring-response {:headers {"Content-Disposition" (str "attachment; filename=" entity_id "_template.csv")}
                       :body (util/render-items ctx items)}))))
 
---
+++ src/kixi/hecuba/data/measurements/download.clj
@@ -0,0 +1,67 @@
+(ns kixi.hecuba.data.measurements.download
+  (:require [kixi.hecuba.storage.db :as db]
+            [qbits.hayt             :as hayt])
+  )
+
+(defn- relocate-customer-ref [m]
+  (-> m
+      (dissoc :metadata)
+      (assoc :customer_ref (get-in m [:metadata :customer_ref]))))
+
+(defn- relocate-location [m]
+  (assoc m :location (get-in m [:location :name])))
+
+(def columns-in-order [:device_id
+                       :type
+                       :customer_ref
+                       :description
+                       :location
+                       :accuracy
+                       :resolution
+                       :frequency
+                       :period
+                       :entity_id ;; NOTE: This is shown as "Parent
+                                  ;; UUID" in the samples. Assuming
+                                  ;; parent_id column is old API and
+                                  ;; using entity_id instead
+                       :max
+                       :min])
+
+(def ^:private extract-columns-in-order (apply juxt columns-in-order))
+
+(def ^:private headers-in-order ["Device UUID"
+                                 "Reading Type"
+                                 "Customer Ref"
+                                 "Description"
+                                 "Location"
+                                 "Accuracy (percent)"
+                                 "Sample Interval (seconds)"
+                                 "Frequency"
+                                 "Period"
+                                 "Parent UUID"
+                                 "Sensor Range Max"
+                                 "Sensor Range Min"])
+
+(defn- join-to-device [devices x]
+  (let [device_id (:device_id x)]
+    (merge (get devices device_id) x)))
+
+(defn get-header [store entity_id]
+  (db/with-session [session (:hecuba-session store)]
+    (let [devices         (->> (db/execute session (hayt/select :devices (hayt/where [[= :entity_id entity_id]])))
+                               (map (fn [x] [(:id x) x]))
+                               (into {}))
+          sensors         (if (seq devices)
+                            (db/execute session (hayt/select :sensors (hayt/where [[:in :device_id (keys devices)]])))
+                            [])
+          device-and-type #(str (:device_id %) "-" (:type %))]
+      (if (seq sensors)
+        (->> sensors
+             (map (partial join-to-device devices))
+             (sort-by device-and-type)
+             (map relocate-customer-ref)
+             (map relocate-location)
+             (map extract-columns-in-order)
+             (apply map vector)
+             (map #(apply vector %1 %2) headers-in-order))
+        []))))
