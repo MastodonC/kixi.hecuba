fa4f9ccdb1b88699292ef39edac7f3ff9fb998e5
Neale Swinnerton <neale@isismanor.com>
2014-09-04 12:34:17 +0100

    use 'our' s3 bucket rather than old embed.

--- dev/photo_migration.clj
+++ dev/photo_migration.clj
@@ -9,14 +9,6 @@
             [clojure.tools.logging            :as log]
             [kixi.hecuba.webutil              :refer (uuid)]))
 
-;; temp hack - we should put this in the config as a separate
-;; component, but this is a throwaway migration so fudge it for now.
-;; you need to fill this in.
-
-(def old-embed-s3-session (s3/mk-session {:access-key ""
-                                          :secret-key ""
-                                          :file-bucket "get-embed-data"
-                                          :download-dir "/tmp"}))
 
 (defn path-from [p]
   (try
@@ -27,27 +19,30 @@
       (log/error (str "parsing " p)))))
 
 ;; TODO - need to look at the path things end up at in s3.
-(defn get-item-from-old-embed-bucket [key]
+(defn get-item-from-old-embed-bucket [s3-session key]
   (let [outfile (ioplus/mk-temp-file! "photo-migration" "")]
     (.deleteOnExit outfile)
     (println key)
-    (when (s3/item-exists? old-embed-s3-session key)
-      (with-open [in (s3/get-object-by-metadata old-embed-s3-session {:key key})]
+    (when (s3/item-exists? s3-session key)
+      (with-open [in (s3/get-object-by-metadata s3-session {:key key})]
         (io/copy in outfile))
       {:src-name  "uploads"
-       :feed-name "photo-migrations"
+       :feed-name "images"
        :uuid      (uuid)
        :dir       (.getParent outfile)
        :filename  (.getName outfile)})))
 
 (defn migrate-photos [{:keys [store]}]
-  (db/with-session [session (:hecuba-session store)]
-    (doseq [{:keys [id photos]} (entities/get-all session)]
-      (log/info "migrating photos for " id)
-      (if-let [paths (not-empty (keep path-from photos))]
-        (doseq [item (keep get-item-from-old-embed-bucket paths)]
-          (log/info "migrating path for " item)
-          (upload/image-upload store  (assoc item :entity_id id)))))))
+  (let [s3 (:s3 store)
+        get-old (partial get-item-from-old-embed-bucket (assoc s3 :file-bucket "get-embed-data"))
+        ]
+    (db/with-session [session (:hecuba-session store)]
+      (doseq [{:keys [id photos]} (entities/get-all session)]
+        (log/info "migrating photos for " id)
+        (if-let [paths (not-empty (keep path-from photos))]
+          (doseq [item (keep get-old paths)]
+            (log/info "migrating path for " item)
+            (upload/image-upload store  (assoc item :entity_id id))))))))
 
 (comment
   ;; from 'user ns after (go)
