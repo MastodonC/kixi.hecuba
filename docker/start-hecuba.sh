1bf8daf75a58ad1d16dc12b9db80b90f088c4f40
Neale Swinnerton <neale@isismanor.com>
2015-03-16 16:59:26 +0000

    Update  start script for c* cluster.

--- docker/start-hecuba.sh
+++ docker/start-hecuba.sh
@@ -5,30 +5,38 @@ CONFIG_FILE=/.hecuba.edn
 DATA_DIR="/data/hecuba/${HOSTNAME}/"
 mkdir -p ${DATA_DIR}
 
-CASSANDRA_SESSION_KEYSPACE=${HECUBA_KEYSPACE?NOT DEFINED}
-S3_FILE_BUCKET=${HECUBA_FILE_BUCKET?NOT DEFINED}
-S3_STATUS_BUCKET=${HECUBA_STATUS_BUCKET?NOT DEFINED}
+CASSANDRA_KEYSPACE_NAME=${HECUBA_CASSANDRA_KEYSPACE_NAME?NOT DEFINED}
+S3_FILE_BUCKET=${HECUBA_S3_FILE_BUCKET?NOT DEFINED}
+S3_STATUS_BUCKET=${HECUBA_S3_STATUS_BUCKET?NOT DEFINED}
+ES_SEARCH_SESSION_NAME=${HECUBA_ES_SEARCH_SESSION_NAME:-hecuba}
+
+contact_points=()
+for i in $(seq 255)
+do
+    cassandra_name=$(printf "CASSANDRA%02d" ${i})
+    cassandra_addr_name="${cassandra_name}_PORT_9042_TCP_ADDR"
+
+    [ ! -z "${!cassandra_addr_name}" ] && contact_points+=("\"${!cassandra_addr_name}\"")
+done
 
 echo "DATA_DIR is ${DATA_DIR}"
-echo "CASSANDRA_SESSION_KEYSPACE is ${CASSANDRA_SESSION_KEYSPACE}"
+echo "CASSANDRA_KEYSPACE_NAME is ${CASSANDRA_KEYSPACE_NAME}"
 echo "S3_FILE_BUCKET is ${S3_FILE_BUCKET}"
 echo "S3_STATUS_BUCKET is ${S3_STATUS_BUCKET}"
+echo "ES_SEARCH_SESSION_NAME is ${ES_SEARCH_SESSION_NAME}"
+echo "CASSANDRA_CLUSTER_CONTACT_POINTS is ${contact_points}"
 
-echo <<EOF > ${CONFIG_FILE}
-
+cat <<EOF > ${CONFIG_FILE}
 {
- :cassandra-session {:keyspace :${CASSANDRA_SESSION_KEYSPACE}
- :hecuba-session {:keyspace ${CASSANDRA_SESSION_KEYSPACE}}
- :search-session {:host "${ES01_TCP_PORT_9200_ADDR}" }
+ :hecuba-session {:keyspace :${CASSANDRA_KEYSPACE_NAME}}
+ :cassandra-cluster {:contact-points [${contact_points[@]}]}
+ :search-session {:host "${ES01_TCP_PORT_9200_ADDR}" :name "${ES_SEARCH_SESSION_NAME}"}
  :s3          {:access-key "${AWS_ACCESS_KEY_ID:?NOT DEFINED}"
                :secret-key "${AWS_SECRET_ACCESS_KEY:?NOT DEFINED}"
                :file-bucket "${S3_FILE_BUCKET}"
                :status-bucket "${S3_STATUS_BUCKET}"
                :download-dir "${DATA_DIR}"}
 }
-
 EOF
 
-export HOME=/
-
-java -jar -Djava.io.tmpdir=${DATA_DIR} /uberjar.jar
+java -jar -Djava.io.tmpdir=${DATA_DIR} -Duser.home=/ /uberjar.jar
